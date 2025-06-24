833b907d6c8c82ccef3b703d648d44617b706d47
Fabrizio Mejia <fmejiaa@csu.fullerton.edu>
2025-06-24 00:04:59 -0700

    games runs with bugs

--- kalah.py
+++ kalah.py
@@ -63,11 +63,19 @@ class Kalah(Game):
                         new_board[index] = 0                      # set own pit to 0
         
         if not again: next_player = 'MIN' if state.to_move == 'MAX' else 'MAX'
+        else: next_player = state.to_move  # stay with the same player if they get another turn
         if next_player == 'MAX':
             moves = [i for i in range(6) if new_board[i] > 0]
         elif next_player == 'MIN':
             moves = [i for i in range(7, 13) if new_board[i] > 0]
 
+        # Sweep 
+        if self.terminal_test(GameState(to_move=next_player, utility=0, moves=moves, board=new_board)):
+            new_board[6] += sum(new_board[:6])  # MAX collects all seeds in its pits
+            new_board[13] += sum(new_board[7:13])  # MIN collects all seeds in its pits
+            for i in range(0, 6): new_board[i] = 0 # set MAX pits to 0
+            for i in range(7, 13): new_board[i] = 0 # set MIN pits to 0
+    
 
         return GameState(to_move=next_player, utility=0, moves=moves, board=new_board)
 
@@ -79,19 +87,26 @@ class Kalah(Game):
             min_score = state.board[13]
             return (max_score - min_score) if player == 'MAX' else (min_score - max_score)
 
+        return 0 # Game is not over, utility is 0
 
     def terminal_test(self, state) -> bool:
         # Check if either player has no seeds left in their pits
         return all(i == 0 for i in state.board[:6]) or all(i == 0 for i in state.board[7:13])
 
     def display(self, state):
-        pass 
+        board = state.board
+        print("Current Board:",board) 
 
 
 
 if __name__ == '__main__':
     kalah = Kalah()
 
-    # utility = kalah.play_game(query_player,alpha_beta_cutoff_search)
+    utility = kalah.play_game(query_player, lambda game, state: alpha_beta_cutoff_search(state, game, d=6))
 
-    # display results 
+    if utility < 0:
+        print("You win!")
+    elif utility > 0:
+        print("You lose!")
+    else:
+        print("It's a draw!")
